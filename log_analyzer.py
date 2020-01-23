31b48600e59067cdc316fb1cda002f85998697bf
Przemyslaw Puczka <p.puczka@gmail.com>
2020-01-23 21:31:28 +0100

    changes

--- log_analyzer.py
+++ log_analyzer.py
@@ -7,6 +7,7 @@ with open("sample_log.log", 'r') as log_file:
 for log in logs:
     print(log)
     if log.__contains__("INFO") and re.search("\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", log):
+       
         with open("filtered_logs_log", 'a') as filtered_log:
             filtered_log.write(log)
         
