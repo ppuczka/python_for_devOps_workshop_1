47957223e6f8ff57ab3c80a1a7b92ab7edaff7a7
Przemyslaw Puczka <p.puczka@gmail.com>
2020-02-04 20:36:55 +0100

    final version

--- game.py
+++ game.py
@@ -6,50 +6,92 @@ computer_moves = {
     3 : "scissors"
 }
 
-def rock():
-    if computer_choice == "rock":
-        print("tie")
-    elif computer_choice == "papper":
-        print("you lost")
-    else:
-        print("you won")
-
-def papper():
-    if computer_choice == "rock":
-        print("you won")
-    elif computer_choice == "papper":
-        print("tie")
-    else:
-        print("you lost")
-
-def scissors():
-    if computer_choice == "rock":
-        print("you lost")
-    elif computer_choice == "papper":
-        print("you won")
-    else:
-        print("tie")
-
-# def user_moves_switcher(usr_arg):
-switcher = {
-    "rock" : rock(),
-    "papper" : papper(),
-    "scissors" : scissors(), 
-    }
+def printResults(arg1, arg2):
+    print("You: " + arg1)
+    print("Computer: " + arg2)
     
-    # return switcher.get(usr_arg, "input invalid")
-    
+
+def rock(arg):
+    if arg == "rock":
+        print("tie")
+    elif arg == "papper":
+        print("you lost")
+    else:
+        print("you won")
+
+def papper(arg):
+    if arg == "rock":
+        print("you won")
+    elif arg == "papper":
+        print("tie")
+    else:
+        print("you lost")
+
+def scissors(arg):
+    if arg == "rock":
+        print("you lost")
+    elif arg == "papper":
+        print("you won")
+    else:
+        print("tie")
 
 def computerInput(): 
     r = random.randint(1, 3)
     return computer_moves.get(r)
-
 user_choice = input("rock / papper / scissors: ")
 while user_choice.lower() != "exit":
     computer_choice = computerInput()
-    print("You: " + user_choice)
-    print("Computer: " + computer_choice)
-    funct = switcher.get(user_choice, "Invalid")
-    user_choice = input("rock / papper / scissors: ")
     
+    if user_choice == "papper":
+        if computer_choice == "papper":
+            printResults(user_choice, computer_choice)
+            print("Tie")
+            user_choice = input("rock / papper / scissors: ")
+            
+        elif computer_choice == "rock":
+            printResults(user_choice, computer_choice)
+            print("You Lost")
+            user_choice = input("rock / papper / scissors: ")
+            
+        else: 
+            printResults(user_choice, computer_choice)
+            print("You Won !!!!!")
+            user_choice = input("rock / papper / scissors: ")
+            
+    elif user_choice == "scissors":  
+        if computer_choice =="rock":
+            printResults(user_choice, computer_choice)
+            print("You lost")
+            user_choice = input("rock / papper / scissors: ")
+            
+        elif computer_choice == "papper":
+            printResults(user_choice, computer_choice)
+            print("You won !!!!!")
+            user_choice = input("rock / papper / scissors: ")
+            
+        else: 
+            printResults(user_choice, computer_choice)
+            print("Tie")
+            user_choice = input("rock / papper / scissors: ")
+            
+    elif user_choice == "rock":    
+        if computer_choice == "papper":
+            printResults(user_choice, computer_choice)
+            print("You lost")
+            user_choice = input("rock / papper / scissors: ")
+            
+        elif computer_choice == "rock":
+            printResults(user_choice, computer_choice)
+            print("Tie")
+            user_choice = input("rock / papper / scissors: ")
+            
+        else: 
+            printResults(user_choice, computer_choice)
+            print("You Won !!!!!")
+            user_choice = input("rock / papper / scissors: ")
+    elif user_choice == "exit":
+        break 
+    else: 
+        print("Wrong choice")
+        user_choice = input("rock / papper / scissors: ")
 
