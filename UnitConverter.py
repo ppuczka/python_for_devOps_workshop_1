45097bd7a150ed75041846aa20904165322785d7
Przemyslaw Puczka <p.puczka@gmail.com>
2020-02-04 23:01:42 +0100

    unit converter

---
+++ UnitConverter.py
@@ -0,0 +1,13 @@
+# this is simple unit converter 
+
+weight_units = {
+    1 : "miligram"
+    2 : "gram"
+    3 : "kilogram"
+    4 : "ton"
+    5 : "ounce"
+    7 : "pound"
+    8 : "stone"
+    9 : "quater"
+    10: "long ton"
+}
