c435bf3cacd0e0f5142ee07a296dbf4da6b3970f
Przemyslaw Puczka <p.puczka@gmail.com>
2020-02-05 22:05:53 +0100

    simple conveter

--- UnitConverter.py
+++ UnitConverter.py
@@ -1,5 +1,8 @@
 # this is simple unit converter 
-
+# add try-catch 
+# add file writting 
+# make it object orienter 
+# work over errors in kg converter 
 def kilogram_converter(unit, x):
     units = {
     "miligram" : lambda x: x / 0.0001,
@@ -21,11 +24,36 @@ def farenheit_to_celsius(f_value):
 
 def weight_converter(input_weight, output_weight):
     weight_unit
-    
-    
-unit = input("Podaj jednostkę: ")
-unit_value = int(input("Podaj ilość kilogramów: ")) 
-   
-result = kilogram_converter(unit, unit_value)
-print(result)
+
+
+def simple_converter():
+    converter = input("""Choose converter: 
+                    1. Celsius to Farenheit
+                    2. Farenheit to Celsius
+                    3. Kilogram converter
+                    Type 1/2/3 or exit() to end program: """)    
+    while converter != "exit()":
+        if converter == "1":
+            c_value = int(input("Type celsius value: "))
+            print(celsius_to_farenheit(c_value))
+        elif converter == "2":
+            f_value = int(input("Type farenheit value: "))
+            print(farenheit_to_celsius(f_value))
+        elif converter == "3":
+            unit = input("Podaj jednostkę: ")
+            unit_value = int(input("Podaj ilość kilogramów: ")) 
+            result = kilogram_converter(unit, unit_value)
+            print(result)
+        elif converter == "exit()":
+            print("Goodbye :)")
+            pass
+        else:
+            print("Wrong choice \n")
+            converter = input("""Choose converter: 
+                    1. Celsius to Farenheit
+                    2. Farenheit to Celsius
+                    3. Kilogram converter
+                    Type 1/2/3 or exit() to end program: """) 
+
+simple_converter()
     
