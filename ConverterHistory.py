c9d1d3d3b65b1f1b156f6d34980d3db387ce7a45
root <root@3ZRKMH2.ra-int.com>
2020-02-07 22:41:34 +0100

    commit

--- ConverterHistory.py
+++ ConverterHistory.py
@@ -5,11 +5,14 @@ class ConverterHistory():
     def __init__(self, history_file_path):
         self.history_file_path = history_file_path
 
-    # @staticmethod
-    def append_to_history(self, write_content):
+    
+    def append_to_history(self, write_contentA, write_contentB, convert_from, convert_to):
         date_time_curent = datetime.datetime.now()
+        current_date_formatted = date_time_curent.strftime("%d-%m-%Y %H:%M:%S")
         
         history_file = open(self.history_file_path, "a")
-        history_file.write(str(date_time_curent) + write_content)
-        print(write_content)
+        history_file.write(f"""Date: {str(current_date_formatted)}:
+            user entered value: {write_contentA} {convert_from}  
+            converted to: {convert_to}
+            result: {write_contentB} \n""")
         
--- UnitConverter.py
+++ UnitConverter.py
@@ -25,32 +25,42 @@ def farenheit_to_celsius(f_value):
 def weight_converter(input_weight, output_weight):
     weight_unit
 
-
 def simple_converter():
     history_file_path = "converter_history.txt"
     history = ConverterHistory(history_file_path)
+    
     try:
+        
         converter = input("""Choose converter: 
                         1. Celsius to Farenheit
                         2. Farenheit to Celsius
                         3. Kilogram converter
                         Type 1/2/3 or exit() to end program: """)    
+        
         while converter != "exit()":
+           
             if converter == "1":
-                c_value = int(input("Type celsius value: "))
-                print(celsius_to_farenheit(c_value))
-                history.append_to_history(str(c_value))
+                entered_unit = "celsius"
+                result_unit = "farenheit"
+                c_value = int(input(f"Type {entered_unit} value: "))
+                f_value = celsius_to_farenheit(c_value)
+                print(f"Result: {f_value} {result_unit}")
+                history.append_to_history(str(c_value), str(f_value), entered_unit, result_unit)
+           
             elif converter == "2":
                 f_value = int(input("Type farenheit value: "))
                 print(farenheit_to_celsius(f_value))
+           
             elif converter == "3":
                 unit = input("Type unit to convert to: ")
                 unit_value = int(input("Type number of kilograms: ")) 
                 result = kilogram_converter(unit, unit_value)
                 print(result)
+           
             elif converter == "exit()":
                 print("Goodbye :)")
                 pass
+           
             else:
                 print("Wrong choice \n")
                 converter = input("""Choose converter: 
@@ -58,6 +68,7 @@ def simple_converter():
                         2. Farenheit to Celsius
                         3. Kilogram converter
                         Type 1/2/3 or exit() to end program: """) 
+    
     except ValueError:
         print("Could not user convert data to an integer.")
 
